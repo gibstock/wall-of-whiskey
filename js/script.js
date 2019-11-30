d5df779065095873864c4645d3173c8283837be0
Gibstock <gibstock@gmail.com>
2019-11-30 15:25:33 -0800

    added iphone screen and recipes

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const cocktailList = ['daiquiri','hot-toddy','mai-tai','manhattan','mojito','moscow-mule','negroni',
+'old-fashioned','whiskey-sour','white-russian'];
+
+x =Math.floor((Math.random() * cocktailList.length));
