e3cb21169c8e7dc7dd028120b9a47f4b574cfeb0
Gibstock <gibstock@gmail.com>
2019-12-01 02:39:25 -0800

    Added new review

--- js/script.js
+++ js/script.js
@@ -1,4 +1,36 @@
-const cocktailList = ['daiquiri','hot-toddy','mai-tai','manhattan','mojito','moscow-mule','negroni',
-'old-fashioned','whiskey-sour','white-russian'];
+const cocktailList = ['daiquiri','hot-toddy',
+'old-fashioned'];
+const whiskeyList = ['Westland-Sherry-Wood', 'Michters-Toasted-Barrel'];
+const leftSide = document.querySelector('.left-side');
+const rightSide = document.querySelector('.right-side');
+const ul = leftSide.querySelector('ul');
+const ulRight = rightSide.querySelector('ul');
 
-x =Math.floor((Math.random() * cocktailList.length));
+
+function displayWhiskey(arr){
+    for(let i=0; i < arr.length; i++){
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        let textNode = document.createTextNode(arr[i]);
+        a.setAttribute('href', `${arr[i]}.html`);
+        a.appendChild(textNode);
+        li.appendChild(a);
+        ul.appendChild(li);
+    }
+}
+
+function displayCocktails(arr){
+    for(let i=0; i < arr.length; i++){
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        let textNode = document.createTextNode(arr[i]);
+        a.setAttribute('href', `${arr[i]}-cocktail-recipe.html`);
+        a.appendChild(textNode);
+        li.appendChild(a);
+        ulRight.appendChild(li);
+    }
+}
+// x =Math.floor((Math.random() * cocktailList.length));
+
+displayCocktails(cocktailList);
+displayWhiskey(whiskeyList);
